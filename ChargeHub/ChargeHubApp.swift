0ab192ccec63be701a6bfca3b1be467c9eb795b4
马林 <boyfighting@qq.com>
2026-06-26 15:57:52 +0800

    Initial Commit

---
+++ ChargeHub/ChargeHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargeHubApp.swift
+//  ChargeHub
+//
+//  Created by mayoyo on 2026/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ChargeHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
